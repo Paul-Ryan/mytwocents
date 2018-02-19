9adf6aa31ac16223fb53686edc380c760e7b1afd
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-02-18 19:48:11 -0800

    Add cookie setter/getter funcions in the cookie_handler.js

---
+++ samples/js/cookie_handler.js
@@ -0,0 +1,37 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(decodeURIComponent(document.cookie));
+});
+
+
+const setCookie = (name,value,days) => {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    console.log(name + "=" + (value || "")  + expires + "; path=/");
+};
+const getCookie = (cookieName) => {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
+const setUserCookie = () => {
+  setCookie('user_id',1,5);
+  console.log(getCookie('user_id'));
+};
