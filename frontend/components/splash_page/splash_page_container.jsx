cd2347bbe271a0fefc4dcc6fb17eae312cb9399b
Paul <paul.ryan.iii@gmail.com>
2018-02-19 17:52:02 -0800

    Create empty entry point

---
+++ frontend/components/splash_page/splash_page_container.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import SplashPage from './splash_page';
+
+const mapStateToProps = (state, ownProps) => {
+
+  return null;
+};
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+
+  return null;
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SplashPage);
