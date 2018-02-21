413d721c4985a364293f2601506e7773f9c47d24
Brian van den Broek <vanden@gmail.com>
2018-02-20 20:02:09 -0800

    Get Hello World for cms rendering

--- backend/config/cms/views.py
+++ backend/config/cms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. I'm the cms index page.")
+
+
 # Create your views here.
