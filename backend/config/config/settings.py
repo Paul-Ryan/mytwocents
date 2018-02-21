b79ccef3d87541c20d677622f766bf0d37efb19d
Brian van den Broek <vanden@gmail.com>
2018-02-21 12:12:51 -0800

    Add skeleton users app with hellow world

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cms',
-    'api'
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
--- backend/config/users/urls.py
+++ backend/config/users/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('users/', include('users.urls')),  
     path('', views.index, name='index'),
 ]
