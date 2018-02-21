ffcfecfb1290fed24a23d6ed3d51399ede7bab0e
Taylor Vann <brian.taylor.vann@gmail.com>
2018-02-21 12:20:00 -0800

    Add user routes

--- backend/config/users/urls.py
+++ backend/config/users/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('create/', views.create, name='create'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('', views.index, name='index'),
 ]
--- backend/config/users/views.py
+++ backend/config/users/views.py
@@ -5,4 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hey users app works")
+    return HttpResponse("Hey users index works")
+
+def create(request):
+    return HttpResponse("Hey users create works")
+
+def login(request):
+    return HttpResponse("Hey users login works")
+
+def logout(request):
+    return HttpResponse("Hey users logout works")
