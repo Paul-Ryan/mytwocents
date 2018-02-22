b596f01b874248b59e0b8514df2ed0fc345f7af8
Taylor Vann <brian.taylor.vann@gmail.com>
2018-02-22 01:36:00 -0800

    Rest easy with restful user login, logout, create api calls

--- backend/config/users/views.py
+++ backend/config/users/views.py
@@ -1,17 +1,63 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+import django.contrib.auth as auth
+
+from .utils import users_session_data
+from .forms import UserForm
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hey users index works")
 
+
 def create(request):
-    return HttpResponse("Hey users create works")
+    if request.method == 'POST' and not request.user.is_authenticated:
+        form = UserForm(request.POST['user'])
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+
+            if user:
+                user.save()
+                auth.login(request, user)
+
+    return JsonResponse(users_session_data(request))
+
 
 def login(request):
-    return HttpResponse("Hey users login works")
+    if request.method == 'POST' and not request.user.is_authenticated:
+        form = UserForm(request.POST['user'])
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(
+                request,
+                username="brian",
+                password="password"
+            )
+
+            if user:
+                auth.login(request, user)
+
+    return JsonResponse(users_session_data(request))
+
 
 def logout(request):
-    return HttpResponse("Hey users logout works")
+    auth.logout(request)
+
+    return JsonResponse(users_session_data(request))
