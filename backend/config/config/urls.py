59605b2a0d02ef8da03f62bbeb146de9e425f190
Brian van den Broek <vanden@gmail.com>
2018-02-22 20:00:26 -0800

    Create restful session and user apis

--- backend/config/config/urls.py
+++ backend/config/config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/session/', include('session.urls')),
     path('', include('cms.urls')),
 ]
