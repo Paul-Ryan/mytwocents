ccfb94e3b0c9e11bca29713289dd78c4d7a59239
Brian van den Broek <vanden@gmail.com>
2018-02-22 20:00:26 -0800

    Create restful session and user apis

--- backend/config/session/utils.py
+++ backend/config/session/utils.py
@@ -1,3 +1,12 @@
+import json
+
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+import django.contrib.auth as auth
+from .forms import SessionForm
+
+
 # Copy and pasted from user/utils.py because of intransigence
 
 def users_session_data(request):
@@ -10,3 +19,29 @@ def users_session_data(request):
     }
 
     return responseData
+
+
+def create_session(request):
+    form = SessionForm(request.POST['user'])
+
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+
+        user = auth.authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user:
+            auth.login(request, user)
+
+    return JsonResponse(users_session_data(request))
+
+
+def delete_session(request):
+    auth.logout(request)
+
+    return JsonResponse(users_session_data(request))
--- backend/config/session/views.py
+++ backend/config/session/views.py
@@ -1,37 +1,9 @@
-from django.http import JsonResponse
-from django.contrib.auth.models import User
+from util .import create_session, delete_session
 
-import django.contrib.auth as auth
-
-from .utils import users_session_data
-from .forms import SessionForm
-
-import json
-
-
-
-def login(request):
+def session(request):
     if request.method == 'POST' and not request.user.is_authenticated:
-        form = SessionForm(request.POST['user'])
-
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-
-            user = auth.authenticate(
-                request,
-                username=username,
-                password=password
-            )
-
-            if user:
-                auth.login(request, user)
-
-    return JsonResponse(users_session_data(request))
+        return create_session(request)
 
 
-def logout(request):
-    auth.logout(request)
-
-    return JsonResponse(users_session_data(request))
+    if request.method == 'DELETE':
+        return delete_session(request)
