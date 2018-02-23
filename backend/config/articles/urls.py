b8c321d8aa80deacc122120463d1db8002785592
Brian van den Broek <vanden@gmail.com>
2018-02-23 10:19:01 -0800

    Add article views import

--- backend/config/articles/urls.py
+++ backend/config/articles/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('', views.article, name="article"),
     path('<int:id>', views.article_by_id, name="article_by_id"),
