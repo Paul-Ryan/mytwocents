983b5b1a5f1dad62448ba1fcab09fa56b7815079
Brian van den Broek <vanden@gmail.com>
2018-02-22 19:18:01 -0800

    Add session urls

--- backend/config/api/urls.py
+++ backend/config/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('session/', include('session.urls')),
     path('users/', include('users.urls')),  
     path('', views.index, name='index'),
 ]
