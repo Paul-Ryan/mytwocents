a719ae6150a681c750eaf9fe9138e53324b2f24b
Paul <paul.ryan.iii@gmail.com>
2018-02-24 14:49:48 -0800

    Create splash page content rows

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -7,40 +7,46 @@ class SplashPage extends React.Component {
   render() {
     return (
       <div>
-        <h2>It works for reader</h2>
+        <h2 className="splash-header">It works for reader</h2>
         <div className="readers">
           <div className="unit">
             <h3>One Click</h3>
             <p>
+              It takes one click, once you're setup your account, anywhere an author has a tip jar button. Tips are a small amount of money, less than the tip you might pay for a coffee, to help writers create content that matters.
             </p>
           </div>
           <div className="unit">
-            <h3>One Click</h3>
+            <h3>Control who you support</h3>
             <p>
+              It takes one click, once you're setup your account, anywhere an author has a tip jar button. Tips are a small amount of money, less than the tip you might pay for a coffee, to help writers create content that matters.
             </p>
           </div>
           <div className="unit">
-            <h3>One Click</h3>
+            <h3>Review your tips</h3>
             <p>
+              It takes one click, once you're setup your account, anywhere an author has a tip jar button. Tips are a small amount of money, less than the tip you might pay for a coffee, to help writers create content that matters.
             </p>
           </div>
         </div>
 
-        <h2>It works for authors</h2>
+        <h2 className="splash-header">It works for authors</h2>
         <div className="authors">
           <div className="unit">
-            <h3>One Click</h3>
+            <h3>Create your Tip Jar</h3>
             <p>
+              It takes one click, once you're setup your account, anywhere an author has a tip jar button. Tips are a small amount of money, less than the tip you might pay for a coffee, to help writers create content that matters.
             </p>
           </div>
           <div className="unit">
-            <h3>One Click</h3>
+            <h3>Pasted it in your article</h3>
             <p>
+              It takes one click, once you're setup your account, anywhere an author has a tip jar button. Tips are a small amount of money, less than the tip you might pay for a coffee, to help writers create content that matters.
             </p>
           </div>
           <div className="unit">
-            <h3>One Click</h3>
+            <h3>Get paid!</h3>
             <p>
+              It takes one click, once you're setup your account, anywhere an author has a tip jar button. Tips are a small amount of money, less than the tip you might pay for a coffee, to help writers create content that matters.
             </p>
           </div>
         </div>
