1ba06e1a9be2bdde6442e49dece49cede336727e
Taylor Vann <brian.taylor.vann@gmail.com>
2018-02-24 14:57:52 -0800

    Change form field to CharField

--- backend/config/articles/forms.py
+++ backend/config/articles/forms.py
@@ -5,9 +5,8 @@ from .validators import validate_non_empty_string
 import articles
 
 class ArticlesForm(ModelForm):
-    title = forms.TextField(validators=[validate_non_empty_string])
-    
+    title = forms.CharField(validators=[validate_non_empty_string])
+
     class Meta:
         model = articles.models.Article
         fields = ["title"]
-        
--- backend/config/articles/models.py
+++ backend/config/articles/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.core.exceptions import ValidationError
-
 from .validators import validate_non_empty_string
 
 
