56bf0071f9e52093a1fca59570d7933273a49d6a
Brian van den Broek <vanden@gmail.com>
2018-02-23 17:59:41 -0800

    Improve article model validations

--- backend/config/articles/models.py
+++ backend/config/articles/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Article(models.Model):
-    
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.TextField(
-    )
+from django.core.exceptions import ValidationError
 
-    
+
+def validate_non_empty_string(value):
+    try:
+        value += ''
+        if value.length == 0:
+            raise ValidationError(
+                ('%(value)s must be a non-empty string'),
+                params={'value': value})
+    except TypeError:
+        raise ValidationError(
+            ('%(value)s is not a string'),
+            params={'value': value})
+
+
+
+class Article(models.Model):
+
+    author_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.TextField(validators=[validate_non_empty_string])
+
+
     class Meta:
         unique_together = (('author_id', 'title'),)
 
     def __str__(self):
         """A string representation of the model."""
         return self.title
-
