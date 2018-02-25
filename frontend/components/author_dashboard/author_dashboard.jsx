1ed39794d5eee44096b6e26327161584fd273980
Paul <paul.ryan.iii@gmail.com>
2018-02-25 15:07:03 -0800

    Add article items to author dash

--- frontend/components/author_dashboard/author_dashboard.jsx
+++ frontend/components/author_dashboard/author_dashboard.jsx
@@ -10,19 +10,21 @@ class AuthorDashboard extends React.Component {
     return (
       <div className="author-info">
         <h1>Your Articles</h1>
-        <ul className="author-article-list">
-          {
-            this.props.articles.map(article => (
-              <ArticleItem
-                article={article}
-                key={article.id}
-                />
-            ))
-          }
-        </ul>
-        <div className="article-meta-info">
-          <h1>40 tips this week</h1>
-          <h1>100 tips total</h1>
+        <div className="author-layout">
+          <ul className="author-article-list">
+            {
+              this.props.articles.map(article => (
+                <ArticleItem
+                  article={article}
+                  key={article.id}
+                  />
+              ))
+            }
+          </ul>
+          <div className="article-meta-info">
+            <h1>40 tips this week</h1>
+            <h1>100 tips total</h1>
+          </div>
         </div>
       </div>
     );
