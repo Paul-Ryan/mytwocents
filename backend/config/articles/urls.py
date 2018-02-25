876cfb089249e6b517364488ea1cf029af767e89
Brian van den Broek <vanden@gmail.com>
2018-02-25 14:42:59 -0800

    Remove comments and unneed methods; fix typo

--- backend/config/articles/urls.py
+++ backend/config/articles/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.article, name="article"),
     path('user/<int:user_id>', views.articles_by_user_id, name="articles_by_user_id"),
-    path('<int:id>', views.article_by_id, name="article_by_id"),
 ]
--- backend/config/articles/views.py
+++ backend/config/articles/views.py
@@ -5,18 +5,8 @@ def article(request):
     if request.method == 'POST':
         return create_article(request)
 
-    if request.method == 'GET':
-        # Should get all the articles of the user, right?
-        return articles_by_user_id(request, user_id)
 
-    # Django really doesn't like None as a return. What to return if there is
-    # no matching request.method? On the one hand, if frontend sends
-    # request.method == 'PATCH' (or, even worse, if it *can* be sent,
-    # request.method == 'FOOBAR'), that's their fault. But, we should arrange
-    # to fail gracefully.
-
-
-def article_by_user_id(request, user_id):
+def articles_by_user_id(request, user_id):
 
     if request.method == 'GET':
         user = User.objects.get(pk=user_id)
