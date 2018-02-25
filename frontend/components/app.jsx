94db72294aec7d1d6956e89abe654d4f701f5a76
Paul <paul.ryan.iii@gmail.com>
2018-02-25 15:47:00 -0800

    Add script form to user page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,6 +12,7 @@ import GlobalNav from './nav_bar/nav_bar_container';
 import SplashPageContainer from './splash_page/splash_page_container';
 import UserPageContainer from './user_page/user_page_container';
 import AuthorDashboardContainer from './author_dashboard/author_dashboard_container';
+import GeneratorFormContainer from './generator_form/script_form_container';
 import { AuthRoute, ProtectRoute } from '../utils/route_utils';
 
 const App = () => (
@@ -20,6 +21,7 @@ const App = () => (
     <ProtectRoute exact path='/dashboard' component={GlobalNav} />
     <ProtectRoute exact path='/dashboard' component={UserPageContainer} />
     <ProtectRoute exact path='/dashboard' component={AuthorDashboardContainer} />
+    <ProtectRoute exact path='/dashboard' component={GeneratorFormContainer} />
 
     <Route exact path="/" component={SplashPageContainer} />
   </div>
