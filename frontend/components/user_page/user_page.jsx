3dac7086dc1f6391a4bb9cd5942544f405ff94fd
Paul <paul.ryan.iii@gmail.com>
2018-02-24 18:27:55 -0800

    Write stuff

--- frontend/components/user_page/user_page.jsx
+++ frontend/components/user_page/user_page.jsx
@@ -14,31 +14,28 @@ class UserPage extends React.Component {
     return (
       <div className="user-info">
         <h1>Your Tips</h1>
-        <div className="all-tips">
-          <h2>Articles you support!</h2>
-          <ul className="user-tip-list">
-            {
-              this.props.tips.map(tip => (
-                <UserTipItem
-                  tip={tip}
-                  key={tip}
-                  removeTip = {this.props.removeTip}
-                  />
-              ))
-            }
-          </ul>
-        </div>
-        <div className="tip-meta-info">
-              <h1>4 tips left</h1>
-              <button>Add more tips</button>
+        <div className="user-layout">
+          <div className="all-tips">
+            <h2>Articles you support!</h2>
+            <ul className="user-tip-list">
+              {
+                this.props.tips.map(tip => (
+                  <UserTipItem
+                    tip={tip}
+                    key={tip}
+                    removeTip = {this.props.removeTip}
+                    />
+                ))
+              }
+            </ul>
+          </div>
+          <div className="tip-meta-info">
+                <h1>4 tips left</h1>
+                <button>Add more tips</button>
+          </div>
         </div>
       </div>
     );
-
-    // render() {
-    //   return (
-    //     <div>{this.props.username}</div>
-    //   );
   }
 }
 
